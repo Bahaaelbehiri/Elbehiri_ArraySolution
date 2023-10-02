ada7db19dd96b52258d483209ff6e5cbe9832708
Bahaa <Bahaa@DESKTOP-FA8A547>
2023-10-02 01:18:57 -0700

    Add project files.

---
+++ Elbehiri_Array/Letters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elbehiri_Array
+{
+    class Charcters
+    {
+        public Char[] Array = new Char[50];
+        char CurrentLetter;
+        bool isFound = false;
+        int CurrentIndex = 0;
+
+        public void SetDataToArry(Char data, int index)
+        {
+            Array[index] = data;
+        }
+
+        public void GetFinalArray(string Characters)
+        {
+            var random = new Random();
+
+            for (int i = 0; i < Characters.Length; i++)
+            {
+                isFound = false;
+                CurrentLetter = Characters[random.Next(Characters.Length)];
+                for (int j = 0; j < 50; j++)
+                {
+                    if (Array[j] == CurrentLetter)
+                    {
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound == false)
+                {
+                    if (CurrentIndex == 50) break;
+                    SetDataToArry(CurrentLetter, CurrentIndex);
+                    CurrentIndex++;
+                }
+            }
+        }
+
+        public void DisplayDataFromArry()
+        {
+            var resultString = new String(Array);
+            Console.WriteLine(resultString);
+        }
+    }
+}
---
+++ Elbehiri_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using Elbehiri_Array;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Elbehiri1Arrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            var Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
+                        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            Charcters ArrayObj = new Charcters();
+            ArrayObj.GetFinalArray(Alpha);
+            ArrayObj.DisplayDataFromArry();
+
+            sw.Stop();
+            Console.WriteLine($"list access time: {sw.Elapsed.ToString()} = {sw.ElapsedMilliseconds.ToString()} ms");
+            Console.ReadKey();
+        }
+    }
+}
